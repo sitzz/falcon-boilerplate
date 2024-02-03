4f13d6d16eb653963fd514eedf953fcccaf38a50
Thomas 'sitzz' Vang <8313369+sitzz@users.noreply.github.com>
2024-02-03 12:22:27 +0100

    chore: moved sqlalchemy manager to own file

--- falcon_boilerplate/__init__.py
+++ falcon_boilerplate/__init__.py
@@ -1,19 +1,1 @@
-import falcon_sqla
-
 __version__ = "0.2.0"
-
-_sqla_manager: falcon_sqla.Manager = None
-
-
-class _SqlaManager:
-    @property
-    def manager(self):
-        return _sqla_manager
-
-    @manager.setter
-    def manager(self, manager):
-        global _sqla_manager
-        _sqla_manager = manager
-
-
-sqla_manager = _SqlaManager()
